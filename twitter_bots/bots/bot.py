0d7ab57739fd364df6dc8ae1d425aa54e452d138
Edmond Lafay <edmond@ogury.co>
2019-11-23 19:07:10 +0100

    Promotion bot done

--- twitter_bots/bots/bot.py
+++ twitter_bots/bots/bot.py
@@ -4,8 +4,8 @@ from ..common.config import config
 
 class Bot:
   name = 'bot'
+  timeout_minutes=60
   thread = None
-  timeout=60
 
   def twitter_auth(self):
     """Authenticate to Twitter"""
@@ -18,37 +18,55 @@ class Bot:
     self.api = tweepy.API(self.twitter_auth(), wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     try:
       self.api.verify_credentials()
-      logging.debug("%s : Authentication OK", self.name)
+      logging.debug(f"{self.name}: Authentication OK")
     except:
-      logging.error("%s : Error during authentication", self.name)
+      logging.error(f"{self.name} : Error during authentication")
 
   def twitbot_timeout(self, log_level, message):
     """Timeout the bot and log the reason"""
-    getattr(logging, log_level)(f"{self.name} : {message}, sleeping {self.timeout} mins")
-    time.sleep(self.timeout * 60)
+    getattr(logging, log_level)(f"{self.name} : {message}, sleeping {self.timeout_minutes} mins")
+    time.sleep(self.timeout_minutes * 60)
 
   def limit_handled(self, cursor):
     """Cursor handling"""
-    while True:
+    tweets_left = True
+    while tweets_left:
       try:
         yield cursor.next()
       except tweepy.RateLimitError:
         self.twitbot_timeout('warn', 'rate limit cursor exceeded')
       except StopIteration:
         self.twitbot_timeout('info', 'Up to date')
+        tweets_left = False
   
-  def post_tweet(self, status='', attachment_url=None, retries=3, **kwargs):
+  def post_tweet(self, retries=3, **kwargs):
     """Post tweet handling"""
     try:
-      logging.info(f"{self.name} : tweeting {status}")
-      self.api.update_status(status=status, attachment_url=attachment_url)
+      logging.info(f"{self.name} : tweeting {kwargs}")
+      self.api.update_status(**kwargs)
+    except tweepy.TweepError as tweep_error:
+      logging.warn(f"{self.name} - post_tweet : {tweep_error}")
     except tweepy.RateLimitError:
       if retries>0:
-        self.twitbot_timeout('warn', 'rate limit exceeded post_tweet')
+        self.twitbot_timeout('warn', 'post_tweet rate limit exceeded')
         self.post_tweet(status, retries=retries-1)
       else:
-        raise Exception('Max retries exceeded for post_tweet')
+        raise Exception(f"{self.name} - post_tweet : Max retries exceeded for post_tweet")
+  
+  def post_retweet(self, tweet, retries=3):
+    """Post retweet handling"""
+    try:
+      self.api.retweet(tweet.id)
+      logging.info(f"{self.name} - post_retweet : https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}")
+    except tweepy.TweepError as tweep_error:
+      logging.warn(f"{self.name} - post_retweet : tweet_id {tweet.id} - {tweep_error}")
+    except tweepy.RateLimitError:
+      if retries>0:
+        self.twitbot_timeout('warn', 'post_retweet rate limit exceeded')
+        self.post_retweet(status, retries=retries-1)
+      else:
+        raise Exception(f"{self.name} - post_retweet : Max retries exceeded for post_tweet")
 
   def run(self):
     """Run the bot"""
-    logging.info("%s : starting OK", self.name)
+    logging.info(f"{self.name} : starting OK")
--- twitter_bots/bots/proper_deals.py
+++ twitter_bots/bots/proper_deals.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import tweepy, time, logging, requests, urllib.request
+import tweepy, time, logging, requests, urllib.request, re
 from bs4 import BeautifulSoup
 from .bot import Bot
 
@@ -7,9 +7,10 @@ from .bot import Bot
 class ProperDeals(Bot):
   def __init__(self):
     self.name = 'proper_deals'
-    super().__init__()
+    self.timeout_minutes=1
     self.shopping_list='shopping'
     self.shopping_author='seigneurcanard'
+    super().__init__()
 
   def get_last_post_tweet_id(self):
     """Get last tweet we computed to avoid spam"""
@@ -21,49 +22,71 @@ class ProperDeals(Bot):
     return 0
 
   def build_status(self, tweet):
-    result = {'attachment_url': f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"}
+    result = {
+      'attachment_url': f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
+    }
     author = tweet.user.screen_name
     url = tweet.entities['urls'][0]['expanded_url']
-    if author=='Dealabs' or author=='ClubicBonsPlans' or author=='bonreduc' or author=='bonsplansastuce' or author=='deal_france':
+    if author in {'Dealabs', 'ClubicBonsPlans', 'bonreduc', 'bonsplansastuce', 'deal_france'}:
       response = requests.get(url, headers={'User-Agent': "PostmanRuntime/7.20.1"})
       if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
+        img_tag = None
+        price_tag= None
         if author=='Dealabs':
-          result['img'] = soup.find('img', 'thread-image')['src']
-          result['text'] = soup.find('span', 'thread-price').string
+          img_tag = soup.find('img', 'thread-image')
+          price_tag = soup.find('span', 'thread-price')
         if author=='ClubicBonsPlans':
-          result['img'] = soup.find('article').find('img')['src']
-          result['text'] = soup.find('article').select("a > b")[0].string
+          tmp_tag = soup.find('article')
+          if tmp_tag:
+            img_tag = tmp_tag.find('img')
+            tmp_tag = soup.find('article')
+            if tmp_tag:
+              tmp_tag = tmp_tag.select("a > b")
+              if tmp_tag:
+                price_tag = tmp_tag[0]
         if author=='bonreduc':
-          result['img'] = soup.find('img', 'odr_img')['src']
-          result['text'] = soup.find('h1').string
+          img_tag = soup.find('img', 'odr_img')
+          price_tag = soup.find('h1')
+        if img_tag and price_tag and 'src' in img_tag:
+          result['img'] = img_tag['src']
+          result['text'] = next(price_tag.stripped_strings)
         if author=='bonsplansastuce':
-          result['text'] = soup.find('span', 'rh_regular_price').string
-          result['img'] = soup.find('img', 'lazyimages')['data-src']
+          img_tag = soup.find('img', 'lazyimages')
+          price_tag = soup.find('span', 'rh_regular_price')
+          if img_tag and price_tag and 'data-src' in img_tag:
+            result['img'] = img_tag['data-src']
+            result['text'] = next(price_tag.stripped_strings)
         if author=='deal_france':
-          result['text'] = next(soup.find('div', 'offer-box-price').stripped_strings)
-          result['img'] = soup.find('img', 'lazyimages')['data-src']
+          img_tag = soup.find('img', 'lazyimages')
+          price_tag = soup.find('div', 'offer-box-price')
+          if img_tag and price_tag and 'data-src' in img_tag:
+            result['img'] = img_tag['data-src']
+            result['text'] = next(price_tag.stripped_strings)
     if 'img' in result and 'text' in result:
-      filename = f"{self.name}imgtmp"
-      request = requests.get(result['img'], stream=True)
-      if request.status_code == 200:
-        with open(filename, 'wb') as image:
-          for chunk in request:
-            image.write(chunk)
-        media = self.api.media_upload(filename)
-        result['media_ids']=[media_id]
-        result['status'] = f"{result['text']}! #promo #bonplan #reduc"
-        self.post_tweet(**(result))
-      else:
-        self.api.retweet(tweet.id)
-    else:
-      self.api.retweet(tweet.id)
-    
+      file_type = re.search(r"\.[a-zA-Z]*$", result['img'])
+      if file_type:
+        filename = f"{self.name}imgtmp{file_type.group()}"
+        request = requests.get(result['img'], stream=True)
+        if request.status_code == 200:
+          with open(filename, 'wb') as image:
+            for chunk in request:
+              image.write(chunk)
+          media = self.api.media_upload(filename)
+          result['media_ids']=[media.media_id]
+          result['status'] = f"{result['text']} #promo #bonplan #reduc #soldes #reduction #promotion"
+          black_friday_tag = re.search(r"#blackfriday", tweet.text, flags=re.IGNORECASE)
+          if black_friday_tag:
+            result['status'] = result['status'] + ' #blackfriday'
+          return self.post_tweet(**(result))
+    return self.post_retweet(tweet)
+
   def run(self):
     logging.info(f"{self.name} : starting OK")
-    last_id = self.get_last_post_tweet_id()
-    for tweet in self.limit_handled(tweepy.Cursor(self.api.list_timeline, owner_screen_name=self.shopping_author, slug=self.shopping_list, since_id=last_id).items(10)):
-      # if the url has a link
-      if len(tweet.entities['urls'])>0:
-        logging.info(f"{self.name} : https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}")
-        self.build_status(tweet)
+    while True:
+      last_id = self.get_last_post_tweet_id()
+      for tweet in self.limit_handled(tweepy.Cursor(self.api.list_timeline, owner_screen_name=self.shopping_author, slug=self.shopping_list, since_id=last_id).items(10)):
+        # if the url has a link
+        if len(tweet.entities['urls'])>0:
+          logging.info(f"{self.name} - run : https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}")
+          self.build_status(tweet)
